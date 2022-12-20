1642b9ad0b60335fce6f8eac4f669dd9b20f89cf
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2022-12-20 13:36:06 +0100

    PDO plus rapide (remplace localhost par 127.0.0.1)

--- WEB/common/bdd.php
+++ WEB/common/bdd.php
@@ -2,7 +2,7 @@
 
 // se connecter à la bdd
 try {
-	$pdo = new PDO("mysql:host=localhost;dbname=maisoneco", "root", "");
+	$pdo = new PDO("mysql:host=127.0.0.1;dbname=maisoneco", "root", "");
 } catch (PDOException $e) {
 	echo $e->getMessage();
 }
