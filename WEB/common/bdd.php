f469eace8686fc439f1681f283de559c103edcfd
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2022-12-25 21:04:37 +0100

    die si pas de bdd (à activer)

--- WEB/common/bdd.php
+++ WEB/common/bdd.php
@@ -5,6 +5,7 @@ try {
 	$pdo = new PDO("mysql:host=127.0.0.1;dbname=maisoneco", "root", "");
 } catch (PDOException $e) {
 	echo $e->getMessage();
+	// die("Impossible de se connecter à la base de données :<br>" . $e->getMessage());
 }
 
 $bdd = $pdo;
