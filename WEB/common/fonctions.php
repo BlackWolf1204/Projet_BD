296c65c3cb5fc3956c688aae06b744a1e21ffcae
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2023-01-07 14:47:33 +0100

    ajoutPieces terminé

---
+++ WEB/common/fonctions.php
@@ -0,0 +1,21 @@
+<?php
+
+function adressePropriete($propriete)
+{
+	$adresse = $propriete['numeroRue'] . " " . $propriete['nomRue'] . ", " . $propriete['codePostal'] . " " . $propriete['ville'];
+	if ($propriete['nomPropriete'] != null) {
+		$labelPropriete = $propriete['nomPropriete'] . " (" . $adresse . ")";
+	} else {
+		$labelPropriete = $adresse;
+	}
+	return $labelPropriete;
+}
+
+function echoLabelPropriete($propriete)
+{
+	$labelPropriete = adressePropriete($propriete);
+	echo "<div id=\"labelPropriete\">";
+	echo "<label for=\"labelPropriete\">Propriété</label> ";
+	echo "<span name=\"labelPropriete\" type=\"text\" readonly=true>$labelPropriete</span>";
+	echo "</div>";
+}
