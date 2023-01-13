f72a9e1a8cabf3c7ffa5444505eea4b0e0e7c717
YasserBelakhdar <yasseyasser55@gmail.com>
2023-01-13 16:28:43 +0100

    13/01

--- WEB/Gestion des appareils/Appareil.php
+++ WEB/Gestion des appareils/Appareil.php
@@ -11,16 +11,27 @@
   FOREIGN KEY(idPiece) REFERENCES Piece(idPiece)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;*/
 session_start();//On démarre la session
+if (isset($_POST['Appareil'])) {
+  $erreur = NULL;
+  if (!empty($_POST)) {
+    require('../common/main.php');
+    if (empty($_POST['nomAppareil']) || empty($_POST['TypeAppareil']) || empty($_POST['emplacement']) ) {
+      $erreur = "Tous les champs doivent être complétés !";
+      if (empty($_POST['nomAppareil'])) {
+          $erreur = "Le nom est vide";
+      } else if (empty($_POST['TypeAppareil'])) {
+          $erreur = "Le type d'appareil est vide";
+      } else if (empty($_POST['emplacement'])) {
+          $erreur = "l'emplacement de l'appareil est vide";
+      }
+      $nomAppareil = htmlspecialchars($_POST['nomAppareil']);
+      $TypeAppareil = htmlspecialchars($_POST['TypeAppareil']);
+      $emplacement = htmlspecialchars($_POST['emplacement']);
 
-if (!empty($_POST)) {
-  require('../common/main.php');
-
-  $nomAppareil = htmlspecialchars($_POST['nomAppareil']);
-  $TypeAppareil = htmlspecialchars($_POST['TypeAppareil']);
-  $emplacement = htmlspecialchars($_POST['emplacement']);
-
-  // Requête SQL
-	$sql = "INSERT INTO Appareil (nomAppareil, TypeAppareil,emplacement) VALUES ('$nomAppareil', '$TypeAppareil','$emplacement')";
+      // Requête SQL
+      $sql = "INSERT INTO Appareil (nomAppareil, TypeAppareil,emplacement) VALUES ('$nomAppareil', '$TypeAppareil','$emplacement')";
+    }
+  } 
 }
 ?>
 
@@ -80,7 +91,7 @@ if (!empty($_POST)) {
                     <div class="row">
                     <div class="col-sm-8 offset-sm-2"><!-- col-sm-8 offset-sm-2 c'est pour le centrage avec un espace de 2 -->
                     <!--on affiche le infos d'appareil nom,  -->
-                    <h2>Profil de <?php echo $userinfo['Prenom']; ?></h2>
+                    <!--<h2>Profil de <?php echo $userinfo['Prenom']; ?></h2>-->
                     <a href="../Page_accueil/Page_accueil.php">Retour</a>
          <form action="Appareil.php" method="post">
                     <!-- Non d'appareil --> 
@@ -108,7 +119,7 @@ if (!empty($_POST)) {
                         <input type="text" name="emplacement" placeholder="Indiquez l'emplacement de votre appareil">
                     </div>
                     <a href="Supprimer.php">Supprimer</a>
-                    <input type="submit" value="Ajouter">
+                    <button type="submit" value="Ajouter"> Ajouter</button>
           </form>
           <?php require('../common/footer.php') ?>
 </body>
