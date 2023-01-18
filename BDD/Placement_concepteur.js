f7504c2745eb1bcecae145686bfac42e8cde6a94
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2023-01-18 22:33:03 +0100

    Placement concepteur màj

--- BDD/Placement_concepteur.js
+++ BDD/Placement_concepteur.js
@@ -1,34 +1,43 @@
 
 // Sauvegarder la position d'elements dans le concepteur de phpMyAdmin
-tables = Array.from(document.querySelectorAll('.pmd_tab'))
+tables = Array.from(document.querySelectorAll('.pma-table'))
 console.log('[' + tables.map(t => `{nom: "${t.id}", left: "${t.style.left}", top: "${t.style.top}"}`).join(',\n') + ']')
 
 
 // Restaurer la position d'elements dans le concepteur de phpMyAdmin
-pos = [{ nom: "maisoneco.administrateur", left: "844px", top: "128px" },
-{ nom: "maisoneco.appareil", left: "516px", top: "580px" },
-{ nom: "maisoneco.appartement", left: "519px", top: "342px" },
-{ nom: "maisoneco.consommer", left: "1058px", top: "552px" },
-{ nom: "maisoneco.historiqueconsommation", left: "258px", top: "691px" },
-{ nom: "maisoneco.immeuble", left: "847px", top: "313px" },
-{ nom: "maisoneco.infopersonne", left: "534px", top: "28px" },
-{ nom: "maisoneco.locataire", left: "613px", top: "206px" },
-{ nom: "maisoneco.piece", left: "233px", top: "300px" },
-{ nom: "maisoneco.produire", left: "1075px", top: "671px" },
-{ nom: "maisoneco.proprietaire", left: "1041px", top: "228px" },
-{ nom: "maisoneco.typeappareil", left: "760px", top: "612px" },
-{ nom: "maisoneco.typeappartement", left: "401px", top: "490px" },
-{ nom: "maisoneco.typepiece", left: "219px", top: "401px" },
-{ nom: "maisoneco.typeressource", left: "1279px", top: "537px" },
-{ nom: "maisoneco.typesecurite", left: "641px", top: "484px" },
-{ nom: "maisoneco.typesubstance", left: "1285px", top: "673px" },
-{ nom: "maisoneco.utilisateur", left: "850px", top: "7px" }];
+pos = [{ nom: "maisoneco.administrateur", left: "1336px", top: "44px" },
+{ nom: "maisoneco.adresse", left: "1050px", top: "440px" },
+{ nom: "maisoneco.appareil", left: "196px", top: "671px" },
+{ nom: "maisoneco.appartement", left: "524px", top: "382px" },
+{ nom: "maisoneco.consommer", left: "927px", top: "658px" },
+{ nom: "maisoneco.departement", left: "1366px", top: "323px" },
+{ nom: "maisoneco.dernierlocataire", left: "170px", top: "470px" },
+{ nom: "maisoneco.dernierproprietaire", left: "228px", top: "22px" },
+{ nom: "maisoneco.historiqueconsommation", left: "183px", top: "832px" },
+{ nom: "maisoneco.infopersonne", left: "1067px", top: "45px" },
+{ nom: "maisoneco.locataire", left: "537px", top: "223px" },
+{ nom: "maisoneco.locataireactuel", left: "210px", top: "46px" },
+{ nom: "maisoneco.piece", left: "206px", top: "379px" },
+{ nom: "maisoneco.produire", left: "929px", top: "816px" },
+{ nom: "maisoneco.proprietaire", left: "837px", top: "228px" },
+{ nom: "maisoneco.proprietaireactuel", left: "131px", top: "140px" },
+{ nom: "maisoneco.propriete", left: "843px", top: "396px" },
+{ nom: "maisoneco.proprieteadresse", left: "42px", top: "311px" },
+{ nom: "maisoneco.region", left: "1175px", top: "337px" },
+{ nom: "maisoneco.rue", left: "1237px", top: "438px" },
+{ nom: "maisoneco.typeappareil", left: "666px", top: "773px" },
+{ nom: "maisoneco.typeappartement", left: "511px", top: "615px" },
+{ nom: "maisoneco.typepiece", left: "200px", top: "499px" },
+{ nom: "maisoneco.typeressource", left: "1179px", top: "668px" },
+{ nom: "maisoneco.typesecurite", left: "521px", top: "536px" },
+{ nom: "maisoneco.typesubstance", left: "1173px", top: "824px" },
+{ nom: "maisoneco.utilisateur", left: "646px", top: "31px" },
+{ nom: "maisoneco.ville", left: "1404px", top: "428px" }];
 
-tables = Array.from(document.querySelectorAll('.pmd_tab'))
+tables = Array.from(document.querySelectorAll('.pma-table'))
 tables.forEach(t => {
 	p = pos.find(p => p.nom == t.id);
-	if (!p) console.log('Pas de position pour ' + t.id);
+	if (!p) { console.log('Pas de position pour ' + t.id); return; }
 	t.style.left = p.left;
 	t.style.top = p.top;
 });
-Re_load();
